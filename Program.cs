5d9fa71632a92cd5f0b7aabd9cc512582abd9107
AndreasBorgstrom <89905856+AndreasBorgstrom@users.noreply.github.com>
2021-10-13 19:39:14 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InheritanceProject
+{
+
+    class Program:Carmanufacturer //run stuff
+    {
+        static void Main()
+        {
+            Console.WriteLine("Type the number of the car brand you want and press enter.");
+            Console.WriteLine("0. Volkswagen, 1. BMW, 2. Audi, 3. Volvo, 4. Ford.");
+            List<string> carBrands = new List<string>();
+            carBrands.Add("vw");
+            carBrands.Add("bmw");
+            carBrands.Add("Audi");
+            carBrands.Add("Volvo");
+            carBrands.Add("Ford");
+            int Brand = int.Parse(Console.ReadLine());
+            Console.ReadKey();
+
+            
+            Console.WriteLine("Now choose the size of the car. You can type either:" +
+                "s (small) " +
+                "m (medium) " +
+                "l (large).");
+            string size = Console.ReadLine();
+            Console.ReadKey();
+            Carmanufacturer ccf = new Carmanufacturer();
+            if (size == "s")
+            {
+                ccf.SmallCarModel(carBrands[Brand]);
+            }
+            else if (size == "m")
+            {
+                ccf.MediumCarModel(carBrands[Brand]);
+            }
+            else if (size == "l")
+            {
+                ccf.BigAssCarModel(carBrands[Brand]);
+            }
+            Console.ReadKey();
+
+
+
+            
+            
+
+            
+
+        }
+    }
+
+}
---
+++ Superclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceProject
+{
+    class Carmanufacturer
+    {
+        public void SmallCarModel(string brand)
+        {
+            Console.WriteLine("This " + brand + " car is tiny");
+        }
+        public void MediumCarModel(string brand)
+        {
+            Console.WriteLine("This " + brand + "car is medium");
+        }
+        public void BigAssCarModel(string brand)
+        {
+            Console.WriteLine("This " + brand + " car is huge. (Good luck paying for gas.)");
+        }
+    }
+}
